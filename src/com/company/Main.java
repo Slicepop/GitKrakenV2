8656392e57a9d5451229c8e10458917291c047f0
Kaden Seeger <02kaden.seeger@gulllakecs.org>
2019-11-25 08:02:36 -0500

    Add CountBlocks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,14 @@
 package com.company;
 
 public class Main {
+    // count blocks
+    public static void countBlocks(int levels) {
+        int total = 0;
+        for (int i = 1; i <= levels; i++) {
+            total = total + (i * i);
+        }
+        System.out.println(total);
+    }
 
     public static void main(String[] args) {
         // this program will create an inner and outer loop
@@ -10,5 +18,6 @@ public class Main {
                 System.out.println("\tInner Loop " + j + ": Hi Everyone!");
             }
         }
+        countBlocks(5);
     }
 }
